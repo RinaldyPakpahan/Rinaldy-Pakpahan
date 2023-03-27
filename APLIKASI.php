0ad0f82ed2883a0fd8be07542eaf733d175b6d39
RinaldyPakpahan <rinaldypakpahan3@gmail.com>
2023-03-27 17:42:43 +0700

    first commit

---
+++ APLIKASI.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>PEMJUMLAHAN</title>
+</head>
+
+<body>
+    <h5> APLIKASI PERHITUNGAN MATEMATIKA </h5>
+    <form action="proses.php" method="POST">
+        Bilangan 1 : <br>
+        <input type="text" name="txtbil1"><br>
+        Bilangan 2 : <br>
+        <input type="text" name="txtbil2"><br>
+        <button type="submit">TAMBAH</button>
+        <button type="submit">KURANG</button>
+        <button type="submit">PERKALIAN</button>
+        <button type="submit">PEMBAGIAN</button>
+    </form>
+</body>
+</html>
